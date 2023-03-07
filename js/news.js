8b339f5b231f3c846846c076997c44ef8409ce4e
seongwoo83 <jeongseongwoo83@daum.net>
2023-03-07 17:20:11 +0900

    03/07 17:20

---
+++ js/news.js
@@ -0,0 +1,17 @@
+window.addEventListener("DOMContentLoaded", ()=>{
+
+    const news  = document.querySelector(".news");
+    const news_body = news.querySelector("tbody");
+
+    
+    let hcode = `<tr>`;
+    for(let i =0;  i<10; i++){
+        hcode += `
+
+        `;
+    }
+
+
+
+
+})
