bc612e54d0e5234fb6b480bc3b0710937696f9f3
seongwoo83 <jeongseongwoo83@daum.net>
2023-03-08 17:00:15 +0900

    03/08 17:00

--- js/news.js
+++ js/news.js
@@ -2,16 +2,33 @@ window.addEventListener("DOMContentLoaded", ()=>{
 
     const news  = document.querySelector(".news");
     const news_body = news.querySelector("tbody");
-
+    console.log(news, news_body);
     
-    let hcode = `<tr>`;
-    for(let i =0;  i<10; i++){
-        hcode += `
-
-        `;
+    let hcode = "";
+    for(let x in ndata){
+            hcode += `<tr>`;
+            for(let y in ndata[x]){
+                if(y === "번호" || y === "작성자"){
+                    hcode += `
+                    <td class="mb_hidden">${ndata[x][y]}</td>
+                `;
+                }else if(y === "제목"){
+                    hcode += `
+                    <td class="tit">
+                        <a href="#">
+                            ${ndata[x][y]}
+                        </a>
+                    </td>
+                    `;
+                }
+                else{
+                    hcode += `
+                    <td>${ndata[x][y]}</td>
+                    `;
+                }
+        }
+        hcode += `</tr>`;
     }
 
-
-
-
+    news_body.innerHTML = hcode;
 })
